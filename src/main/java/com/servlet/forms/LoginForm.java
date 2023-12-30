5b029610d7151c1bf2fb359a273661b55c1c9aee
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-30 14:12:08 +0530

    added profile.jsp

--- src/main/java/com/servlet/forms/LoginForm.java
+++ src/main/java/com/servlet/forms/LoginForm.java
@@ -3,6 +3,7 @@ package com.servlet.forms;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,6 +19,7 @@ public class LoginForm extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// supports both get & post,
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String name = req.getParameter("name");
@@ -25,8 +27,40 @@ public class LoginForm extends HttpServlet{
 		String password = req.getParameter("password");
 		
 		PrintWriter pw = resp.getWriter();
-					pw.write("Name "+name);
-					pw.write("E-Mail "+mail);
-					pw.write("Password "+password);
+					pw.write("\nName "+name);
+					pw.write("\nE-Mail "+mail);
+					pw.write("\nPassword "+password);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+		String name = req.getParameter("name");
+		String mail = req.getParameter("mail");
+		String password = req.getParameter("password");
+		
+		PrintWriter pw = resp.getWriter();
+					pw.write("\nName "+name);
+					pw.write("\nE-Mail "+mail);
+					pw.write("\nPassword "+password);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		super.doGet(req, resp);
+		// request objet,
+		String name = req.getParameter("name");
+		String password = req.getParameter("password");
+			
+		if(name.equalsIgnoreCase("Test") || password.equalsIgnoreCase("tesT"))
+		{
+			RequestDispatcher rd = req.getRequestDispatcher("/profile.jsp");
+								rd.forward(req, resp);
+		}
+		else
+		{
+			RequestDispatcher rd = req.getRequestDispatcher("/index.html");
+			rd.forward(req, resp);
+		}
 	}
 }
